0fd71c2a5d9384944eb1ea18bf4aa930a40107fb
Sam Macbeth <sam@cliqz.com>
2019-11-29 14:31:58 +0100

    Add dat-archive module

---
+++ packages/dat-archive/lib/dat-archive.ts
@@ -0,0 +1,34 @@
+import DatArchiveImpl = require('@sammacbeth/dat-node/lib/dat-archive');
+import Hyperdrive from '@sammacbeth/dat-types/lib/hyperdrive';
+
+export type TimeoutOption = {
+  timeout?: number;
+};
+
+/**
+ * TODO: This is not a complete spec yet...
+ */
+export interface IDatArchive {
+  url: string;
+  getInfo(opts?: TimeoutOption): Promise<any>;
+  configure(opts?: any): Promise<void>;
+  stat(filepath: string, opts?: TimeoutOption): Promise<any>;
+  readFile(path: string, opts?: TimeoutOption | any): Promise<string | ArrayBuffer>;
+  readdir(path: string, opts?: TimeoutOption | any): Promise<string[]>;
+  writeFile(path: string, data: string | ArrayBuffer, opts?: any): Promise<void>;
+  mkdir(path: string): Promise<void>;
+  unlink(path: string): Promise<void>;
+  rmdir(path: string, opts?: any): Promise<void>;
+  copy(path: string, dstPath: string, opts?: any): Promise<void>
+  rename(oldPath: string, newPath: string, opts?: any): Promise<void>
+  history(opts?: any): Promise<void>
+  checkout(version: number): void
+  download(path: string, opts?: TimeoutOption): Promise<void>
+}
+
+export default function createDatArchive(drive: Hyperdrive): IDatArchive {
+  return DatArchiveImpl({
+    _dataStructure: drive,
+    key: drive.key.toString(),
+  });
+}
