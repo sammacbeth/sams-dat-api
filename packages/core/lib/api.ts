29859dfa9e533320ead6de297ea293339f01eb1c
Sam Macbeth <sam@cliqz.com>
2019-12-03 13:37:40 +0100

    Add events to the API.

--- packages/core/lib/api.ts
+++ packages/core/lib/api.ts
@@ -1,16 +1,29 @@
 import { IHyperdrive } from '@sammacbeth/dat-types/lib/hyperdrive';
+import { EventEmitter } from 'events';
+import StrictEventEmitter from 'strict-event-emitter-types';
 import Dat from './dat';
 import DatLoaderBase, { LoadOptions } from './loader';
+
+// Events emitted by the API
+interface IAPIEvents {
+  load: (dat: Dat<IHyperdrive>) => void;
+  use: (dat: Dat<IHyperdrive>) => void;
+  create: (dat: Dat<IHyperdrive>) => void;
+  delete: (address: string) => void;
+}
 
 export type SwarmOptions = {
   autoSwarm?: boolean;
 };
 
-export default class HyperdriveAPI<D extends IHyperdrive> {
+export default class HyperdriveAPI<
+  D extends IHyperdrive
+> extends (EventEmitter as new () => StrictEventEmitter<EventEmitter, IAPIEvents>) {
   public dats = new Map<string, Dat<D>>();
   public loader: DatLoaderBase<D>;
 
   constructor(loader: DatLoaderBase<D>) {
+    super();
     this.loader = loader;
   }
 
@@ -24,12 +37,14 @@ export default class HyperdriveAPI<D extends IHyperdrive> {
     if (this.dats.has(address)) {
       const datFromCache = this.dats.get(address);
       handleAutoJoin(datFromCache);
+      this.emit('use', datFromCache);
       return datFromCache;
     }
     const dat = await this.loader.load(Buffer.from(address, 'hex'), options);
     this.dats.set(address, dat);
     handleAutoJoin(dat);
     dat.on('close', () => this.dats.delete(address));
+    this.emit('load', dat);
     return dat;
   }
 
@@ -42,6 +57,7 @@ export default class HyperdriveAPI<D extends IHyperdrive> {
     const address = dat.drive.key.toString('hex');
     this.dats.set(address, dat);
     dat.on('close', () => this.dats.delete(address));
+    this.emit('create', dat);
     return dat;
   }
 
@@ -50,6 +66,7 @@ export default class HyperdriveAPI<D extends IHyperdrive> {
       const dat = this.dats.get(address);
       dat.close();
     }
+    this.emit('delete', address);
     return this.loader.delete(address);
   }
 
