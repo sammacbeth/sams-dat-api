9b237d7902045e46e024856c2e966c09d50df95c
Sam Macbeth <sam@macbeth.cc>
2019-12-04 16:40:43 +0000

    Implement DatArchive create and fork

--- packages/dat-archive/lib/dat-archive.ts
+++ packages/dat-archive/lib/dat-archive.ts
@@ -1,9 +1,19 @@
+import { AbstractHyperdriveAPI, SwarmOptions } from '@sammacbeth/dat-api-core/lib/api';
 import DatArchiveImpl = require('@sammacbeth/dat-node/lib/dat-archive');
 import { IHyperdrive } from '@sammacbeth/dat-types/lib/hyperdrive';
+import pda from 'pauls-dat-api';
 
 export type TimeoutOption = {
   timeout?: number;
 };
+
+export type DatArchiveCreationOptions = {
+  title?: string;
+  description?: string;
+  type?: string[];
+  links?: any;
+  prompt?: boolean;
+}
 
 /**
  * TODO: This is not a complete spec yet...
@@ -19,11 +29,50 @@ export interface IDatArchive {
   mkdir(path: string): Promise<void>;
   unlink(path: string): Promise<void>;
   rmdir(path: string, opts?: any): Promise<void>;
-  copy(path: string, dstPath: string, opts?: any): Promise<void>
-  rename(oldPath: string, newPath: string, opts?: any): Promise<void>
-  history(opts?: any): Promise<void>
-  checkout(version: number): void
-  download(path: string, opts?: TimeoutOption): Promise<void>
+  copy(path: string, dstPath: string, opts?: any): Promise<void>;
+  rename(oldPath: string, newPath: string, opts?: any): Promise<void>;
+  history(opts?: any): Promise<void>;
+  checkout(version: number): void;
+  download(path: string, opts?: TimeoutOption): Promise<void>;
+}
+
+export async function create(
+  node: AbstractHyperdriveAPI,
+  datOpts: { persist: boolean } & SwarmOptions,
+  manifest: DatArchiveCreationOptions,
+): Promise<IDatArchive> {
+  const dat = await node.createDat(datOpts);
+  await pda.writeManifest(dat.drive, manifest);
+  return createDatArchive(dat.drive);
+}
+
+export async function fork(
+  node: AbstractHyperdriveAPI,
+  srcDrive: IHyperdrive,
+  datOpts: { persist: boolean } & SwarmOptions,
+  manifest: DatArchiveCreationOptions,
+): Promise<IDatArchive> {
+  const srcManifest = await pda.readManifest(srcDrive).catch((_) => ({}));
+  // create a new manifest with overrides
+  const dstManifest = {
+    description: manifest.description ? manifest.description : srcManifest.description,
+    title: manifest.title ? manifest.title : srcManifest.title,
+    type: manifest.type ? manifest.type : srcManifest.type,
+  };
+  ['web_root', 'fallback_page', 'links'].forEach((field) => {
+    if (srcManifest[field]) {
+      dstManifest[field] = srcManifest[field];
+    }
+  });
+  const dstDat = await node.createDat(datOpts);
+  await pda.writeManifest(dstDat.drive, dstManifest);
+  await pda.exportArchiveToArchive({
+    dstArchive: dstDat.drive,
+    ignore: ['/.dat', '/.git', '/dat.json'],
+    skipUndownloadedFiles: false,
+    srcArchive: srcDrive,
+  });
+  return createDatArchive(dstDat.drive);
 }
 
 export default function createDatArchive(drive: IHyperdrive): IDatArchive {
