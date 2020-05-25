7ed836c5c491dcab33ee48f1f9c65b361addcbdd
Sam Macbeth <sam@cliqz.com>
2020-05-25 16:47:05 +0200

    Use a single corestore for all drives.

--- packages/dat2/lib/index.ts
+++ packages/dat2/lib/index.ts
@@ -3,10 +3,10 @@ import { Hyperdrive10 } from '@sammacbeth/dat-types/lib/hyperdrive10';
 import { IReplicableNoise } from '@sammacbeth/dat-types/lib/replicable';
 import ISwarm, { JoinSwarmOptions } from '@sammacbeth/dat-types/lib/swarm';
 import { EventEmitter } from 'events';
-import HypercoreProtocol = require('hypercore-protocol');
 import hyperdrive10Impl = require('hyperdrive');
-import hyperswarm = require('hyperswarm');
-import pump = require('pump');
+import Corestore = require('corestore');
+import ram = require('random-access-memory');
+import SwarmNetworker = require('corestore-swarm-networking');
 
 export type HyperswarmOpts = {
   bootstrap?: string[];
@@ -25,76 +25,54 @@ export type CoreStoreOpts = {
 
 class HyperSwarm<T extends IReplicableNoise> extends EventEmitter implements ISwarm<T> {
   private swarm: any;
-  private feeds = new Map<string, [IReplicableNoise, JoinSwarmOptions]>();
+  private ready: Promise<void>
 
-  constructor(opts?: HyperswarmOpts) {
+  constructor(loader: CorestoreLoader, opts?: HyperswarmOpts) {
     super();
-    this.swarm = hyperswarm(opts);
-    this.swarm.on('connection', (connection, info) => {
-      if (info.peer && this.feeds.has(info.peer.topic.toString('hex'))) {
-        // if peer is provided we can replicate directly
-        const [feed, feedOpts] = this.feeds.get(info.peer.topic.toString('hex'));
-        const stream = feed.replicate(!!info.client, feedOpts);
-        pump(connection, stream, connection);
-      } else {
-        const stream = new HypercoreProtocol(!!info.client, { live: true, encrypt: true });
-        stream.on('handshake', () => {
-          const deduped = info.deduplicate(stream.publicKey, stream.remotePublicKey);
-          if (deduped) {
-            return;
-          }
-          stream.on('discovery-key', (dbuf: Buffer) => {
-            const dkey = dbuf.toString('hex');
-            if (this.feeds.has(dkey)) {
-              const [feed, feedOpts] = this.feeds.get(dkey);
-              // TODO: can we do this with a public API?
-              (feed as any).corestore.replicate(!!info.client, {
-                ...feedOpts,
-                stream,
-              });
-            }
-          });
-        });
-        pump(connection, stream, connection);
-      }
-    });
+    this.ready = new Promise(async (resolve) => {
+      await loader.ready;
+      this.swarm = new SwarmNetworker(loader.corestore, opts);
+      resolve();
+    })
   }
 
   public async add(feed: IReplicableNoise, options: JoinSwarmOptions = {}) {
     const { announce = false, lookup = true, upload = true, download = true } = options;
     const opts = { announce, lookup, upload, download };
-    feed.ready(() => {
-      this.swarm.join(feed.discoveryKey, opts);
-    });
-    this.feeds.set(feed.discoveryKey.toString('hex'), [feed, opts]);
+    await this.ready;
+    await this.swarm.join(feed.discoveryKey, opts);
   }
 
   public remove(feed: IReplicableNoise) {
     this.swarm.leave(feed.discoveryKey);
-    this.feeds.delete(feed.discoveryKey.toString('hex'));
   }
 
   public close() {
-    return this.swarm.destroy();
+    return this.swarm.close();
   }
 }
 
 class CorestoreLoader extends DatLoaderBase<Hyperdrive10> {
   public corestore: any;
-  public tmpstore: any;
+  public ready: Promise<void>
 
   constructor(opts: StorageOpts & HyperswarmOpts) {
     super({
       hyperdriveFactory: (storage, key, driveOpts) => {
-        return hyperdrive10Impl(storage, key, driveOpts);
+        return hyperdrive10Impl(this.corestore, key, driveOpts);
       },
-      swarmFactory: () => new HyperSwarm(opts),
+      swarmFactory: () => new HyperSwarm(this, opts),
       ...opts,
     });
-  }
-
-  public suspend(): void {
-    super.suspend();
+    this.ready = new Promise(async (resolve) => {
+      if (opts.persistantStorageFactory) {
+        const storage = await opts.persistantStorageFactory('default');
+        this.corestore = new Corestore(storage, opts);
+      } else {
+        this.corestore = new Corestore(ram, opts);
+      }
+      resolve();
+    })
   }
 }
 
